0ca55dc2eca7904a403a954a6988f88de72a23fb
Mustafa Salih ASLIM <salih@aslim.net>
2024-09-09 01:01:40 +0300

    renamed the DbContext property

--- src/SampleDotnet.RepositoryFactory/Repositories/Implementations/Repository.cs
+++ src/SampleDotnet.RepositoryFactory/Repositories/Implementations/Repository.cs
@@ -4,13 +4,13 @@
 /// A generic repository class that provides CRUD operations and additional methods for querying and manipulating entities.
 /// Inherits from <see cref="RepositoryBase"/> and implements <see cref="IRepository{TDbContext}"/>.
 /// </summary>
-/// <typeparam name="TDbContext">The type of the _context.</typeparam>
+/// <typeparam name="TDbContext">The type of the Db.</typeparam>
 internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbContext> where TDbContext : DbContext
 {
     // The DbContext used by the repository.
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Repository{TDbContext}"/> class with the specified _context.
+    /// Initializes a new instance of the <see cref="Repository{TDbContext}"/> class with the specified Db.
     /// </summary>
     /// <param name="dbContext">The DbContext instance.</param>
     internal Repository(TDbContext dbContext)
@@ -25,7 +25,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     /// <returns>An IQueryable of the specified entity type.</returns>
     public IQueryable<T> AsNoTracking<T>() where T : class
     {
-        return _context.Set<T>().AsNoTracking();
+        return Db.Set<T>().AsNoTracking();
     }
 
     /// <summary>
@@ -35,7 +35,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     /// <returns>An IQueryable of the specified entity type.</returns>
     public IQueryable<T> AsQueryable<T>() where T : class
     {
-        return _context.Set<T>();
+        return Db.Set<T>();
     }
 
     /// <summary>
@@ -47,7 +47,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     public T? Find<T>(params object[] keyValues) where T : class
     {
         ArgumentNullException.ThrowIfNull(keyValues, nameof(keyValues));
-        return _context.Set<T>().Find(keyValues);
+        return Db.Set<T>().Find(keyValues);
     }
 
     /// <summary>
@@ -60,7 +60,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     public ValueTask<T?> FindAsync<T>(object[] keyValues, CancellationToken cancellationToken = default) where T : class
     {
         ArgumentNullException.ThrowIfNull(keyValues, nameof(keyValues));
-        return _context.Set<T>().FindAsync(keyValues, cancellationToken);
+        return Db.Set<T>().FindAsync(keyValues, cancellationToken);
     }
 
     /// <summary>
@@ -72,7 +72,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     public T? FirstOrDefault<T>(Expression<Func<T, bool>> predicate) where T : class
     {
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
-        return _context.Set<T>().FirstOrDefault(predicate);
+        return Db.Set<T>().FirstOrDefault(predicate);
     }
 
     /// <summary>
@@ -82,7 +82,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     /// <returns>The first entity in the sequence, or null.</returns>
     public T? FirstOrDefault<T>() where T : class
     {
-        return _context.Set<T>().FirstOrDefault();
+        return Db.Set<T>().FirstOrDefault();
     }
 
     /// <summary>
@@ -95,7 +95,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     public Task<T?> FirstOrDefaultAsync<T>(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default) where T : class
     {
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
-        return _context.Set<T>().FirstOrDefaultAsync(predicate, cancellationToken);
+        return Db.Set<T>().FirstOrDefaultAsync(predicate, cancellationToken);
     }
 
     /// <summary>
@@ -106,7 +106,7 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     /// <returns>A task representing the asynchronous operation, with the first entity in the sequence, or null.</returns>
     public Task<T?> FirstOrDefaultAsync<T>(CancellationToken cancellationToken = default) where T : class
     {
-        return _context.Set<T>().FirstOrDefaultAsync(cancellationToken);
+        return Db.Set<T>().FirstOrDefaultAsync(cancellationToken);
     }
 
     /// <summary>
@@ -118,6 +118,6 @@ internal sealed class Repository<TDbContext> : RepositoryBase, IRepository<TDbCo
     public IQueryable<TEntity> Where<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
     {
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
-        return _context.Set<TEntity>().Where(predicate);
+        return Db.Set<TEntity>().Where(predicate);
     }
 }
